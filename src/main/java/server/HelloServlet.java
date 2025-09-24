a4c832c690eaf526c7ed5778ad048a457c683870
kemalthes <kemalmutalup@gmail.com>
2025-09-24 23:16:15 +0300

    class work 2

--- src/main/java/server/HelloServlet.java
+++ src/main/java/server/HelloServlet.java
@@ -1,6 +1,5 @@
 package server;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,27 +11,27 @@ import java.util.stream.Collectors;
 @WebServlet(name = "http", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
         writer.println("Hello world");
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintWriter writer = resp.getWriter();
         String body = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
         writer.println("POST запрос выполнен: " + body);
     }
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintWriter writer = resp.getWriter();
         String body = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
         writer.println("PUT запрос выведен: " + body);
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintWriter writer = resp.getWriter();
         String body = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
         writer.println("DELETE запрос выполнен: " + body);
